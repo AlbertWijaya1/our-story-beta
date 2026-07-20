05319f623643d28ab214ab1c39eae0f99fa21298
Albert <albertwijaya48@gmail.com>
2026-07-21 03:05:28 +0700

    fix wording

--- data/scenes.js
+++ data/scenes.js
@@ -44,7 +44,7 @@ const scenes = {
           "images/everyday-us/kite-left-srgb.jpg",
           "images/everyday-us/kite-right.jpg"
         ],
-        text: "Not because the days were special. Because you were there."
+        text: "Not because the days were special. It's because you were there."
       }
     ]
   },  
